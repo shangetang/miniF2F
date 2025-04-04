abf26babdc6dfbf8501a4881ff7efba4dd8a20ed
shangetang <sangertang1999@gmail.com>
2025-04-03 23:54:59 -0400

    make the lakefile.lean compatible with deepseek

--- lakefile.lean
+++ lakefile.lean
@@ -12,16 +12,28 @@ package «miniF2F» {
   -- add any package configuration options here
 }
 
+-- require mathlib from git
+--   "https://github.com/leanprover-community/mathlib4.git"
+
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
+  "https://github.com/xinhjBrant/mathlib4.git" @ "2f65ba7f1a9144b20c8e7358513548e317d26de1"
 
 -- require mathlib from git
 --   "https://github.com/leanprover-community/mathlib4.git" @ "v4.0.0"
 
-require aesop from git "https://github.com/leanprover-community/aesop" @ "master"
--- require proofwidgets from git "https://github.com/leanprover-community/ProofWidgets4" @ "v0.0.36"
-require proofwidgets from git "https://github.com/leanprover-community/ProofWidgets4" @ "v0.0.30"
+-- require aesop from git "https://github.com/leanprover-community/aesop" @ "master"
+-- -- require proofwidgets from git "https://github.com/leanprover-community/ProofWidgets4" @ "v0.0.36"
+-- require proofwidgets from git "https://github.com/leanprover-community/ProofWidgets4" @ "v0.0.30"
 
+
+
+require batteries from git "https://github.com/leanprover-community/batteries" @ "main"
+require Qq from git "https://github.com/leanprover-community/quote4" @ "master"
+require aesop from git "https://github.com/leanprover-community/aesop" @ "master"
+require proofwidgets from git "https://github.com/leanprover-community/ProofWidgets4" @ "v0.0.36"
+require Cli from git "https://github.com/leanprover/lean4-cli" @ "main"
+require importGraph from git "https://github.com/leanprover-community/import-graph.git" @ "main"
+require REPL from git "https://github.com/xinhjBrant/repl.git" @ "deepseek"
 
 @[default_target]
 lean_lib «MiniF2F» where
