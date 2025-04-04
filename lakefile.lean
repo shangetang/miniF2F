93b8635f69475654347b712edcade2534f87104b
shangetang <sangertang1999@gmail.com>
2025-04-04 00:04:30 -0400

    lakefile

--- lakefile.lean
+++ lakefile.lean
@@ -27,13 +27,13 @@ require mathlib from git
 
 
 
-require batteries from git "https://github.com/leanprover-community/batteries" @ "main"
-require Qq from git "https://github.com/leanprover-community/quote4" @ "master"
-require aesop from git "https://github.com/leanprover-community/aesop" @ "master"
-require proofwidgets from git "https://github.com/leanprover-community/ProofWidgets4" @ "v0.0.36"
-require Cli from git "https://github.com/leanprover/lean4-cli" @ "main"
-require importGraph from git "https://github.com/leanprover-community/import-graph.git" @ "main"
-require REPL from git "https://github.com/xinhjBrant/repl.git" @ "deepseek"
+-- require batteries from git "https://github.com/leanprover-community/batteries" @ "main"
+-- require Qq from git "https://github.com/leanprover-community/quote4" @ "master"
+-- require aesop from git "https://github.com/leanprover-community/aesop" @ "master"
+-- require proofwidgets from git "https://github.com/leanprover-community/ProofWidgets4" @ "v0.0.36"
+-- require Cli from git "https://github.com/leanprover/lean4-cli" @ "main"
+-- require importGraph from git "https://github.com/leanprover-community/import-graph.git" @ "main"
+-- require REPL from git "https://github.com/xinhjBrant/repl.git" @ "deepseek"
 
 @[default_target]
 lean_lib «MiniF2F» where
