35fce3192c4985e48bcdee213237cd7473c2863d
shangetang <sangertang1999@gmail.com>
2025-04-03 21:43:40 -0400

    add import the files we want to trace in MiniF2F.lean

--- MiniF2F.lean
+++ MiniF2F.lean
@@ -1,3 +1,8 @@
 -- This module serves as the root of the `MiniF2F` library.
 -- Import modules here that should be built as part of the library.
 import «MiniF2F».Basic
+
+import MiniF2F.correct_answers_CoT_Test
+import MiniF2F.correct_answers_Goedel_Test
+import MiniF2F.Test
+import MiniF2F.Validation
