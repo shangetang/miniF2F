dbabbcbaea9aeb533dae034fbef586a431ba83e6
shangetang <sangertang1999@gmail.com>
2025-04-03 23:36:23 -0400

    lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -1,12 +1,16 @@
 import Lake
 open Lake DSL
 
-package «miniF2F» where
-  -- Settings applied to both builds and interactive editing
-  leanOptions := #[
-    ⟨`pp.unicode.fun, true⟩ -- pretty-prints `fun a ↦ b`
-  ]
-  -- add any additional package configuration options here
+-- package «miniF2F» where
+--   -- Settings applied to both builds and interactive editing
+--   leanOptions := #[
+--     ⟨`pp.unicode.fun, true⟩ -- pretty-prints `fun a ↦ b`
+--   ]
+--   -- add any additional package configuration options here
+
+package «miniF2F» {
+  -- add any package configuration options here
+}
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
