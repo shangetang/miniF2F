11472a0234f4efae4e2197645a60c50e1232dfb5
shangetang <sangertang1999@gmail.com>
2025-04-03 23:43:25 -0400

    lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -15,8 +15,13 @@ package «miniF2F» {
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
 
+-- require mathlib from git
+--   "https://github.com/leanprover-community/mathlib4.git" @ "v4.0.0"
+
 require aesop from git "https://github.com/leanprover-community/aesop" @ "master"
-require proofwidgets from git "https://github.com/leanprover-community/ProofWidgets4" @ "v0.0.36"
+-- require proofwidgets from git "https://github.com/leanprover-community/ProofWidgets4" @ "v0.0.36"
+require proofwidgets from git "https://github.com/leanprover-community/ProofWidgets4" @ "v0.0.30"
+
 
 @[default_target]
 lean_lib «MiniF2F» where
